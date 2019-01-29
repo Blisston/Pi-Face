66e1a84c66a9bb2b351d09a6095f14b508ed7a8a
Blisston kirubha <blisstonkirubha@gmail.com>
2019-01-30 00:43:12 +0530

    added login functionality

--- piface/src/app/app.module.ts
+++ piface/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { FeedComponent } from './feed/feed.component';
 import { DetailsService} from './details.service';
 import {HttpModule} from '@angular/http';
+import { LoginComponent } from './login/login.component';
+import { SavedComponent } from './saved/saved.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeedComponent
+    FeedComponent,
+    LoginComponent,
+    SavedComponent
   ],
   imports: [
     BrowserModule,
